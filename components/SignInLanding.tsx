80e85827d3f370d1aed6cbf20330006a5ac096d9
Brian Walters <waltersbrian68@gmail.com>
2023-08-09 13:41:50 -0400

    finsished log in page for now

--- components/SignInLanding.tsx
+++ components/SignInLanding.tsx
@@ -1,18 +1,29 @@
 "use client";
 
+import Image from "next/image";
+import googleLogo from "../public/google-logo.png";
 import { signIn } from "next-auth/react";
 
 export default function SignInLanding() {
   return (
-    <div className="m-auto mt-20 bg-slate-500 max-w-lg px-4">
-      <h1>Welcome to BP4!</h1>
+    <div className="m-auto mt-52 max-w-lg px-4 flex flex-col items-center">
+      <h1 className="font-bold text-3xl text-center">Log in</h1>
       <button
-        className="text-2xl font-semibold"
+        className="text-lg font-semibold border-2 px-10 py-2 flex mt-8 rounded-lg hover:shadow"
         onClick={() => {
           signIn("google");
         }}
       >
-        Sign in with Google
+        <Image
+          className="mr-2"
+          src={googleLogo}
+          alt="Google Logo"
+          width={30}
+          // height={500} automatically provided
+          // blurDataURL="data:..." automatically provided
+          // placeholder="blur" // Optional blur-up while loading
+        />
+        Continue with Google
       </button>
     </div>
   );
